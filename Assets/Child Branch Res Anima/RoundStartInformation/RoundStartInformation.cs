0d5e6f52be73a54b568339ad386938d3b72d0024
StephanStelter <raszs3r@googlemail.com>
2025-11-02 16:02:43 +0100

    new

---
+++ Assets/Child Branch Res Anima/RoundStartInformation/RoundStartInformation.cs
@@ -0,0 +1,228 @@
+﻿
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class RoundStartInformation : MonoBehaviour
+
+{
+    //UNITY IST KACKE TESTESTEST
+    public static RoundStartInformation Instance { get; set; }
+
+
+    [System.Serializable]
+    public class ResourcesPerRound
+    {
+        public ResourceType resourceType;
+        public int amount;
+        public Sprite sprite;
+        public RessourceField ressourceField;
+    }
+
+    public List<ResourcesPerRound> resourceListPerRoundMainPLayer = new List<ResourcesPerRound>() { };
+
+    [System.Serializable]
+    public class ResourcesPerSign
+    {
+        public Sign sign;
+        public int woodAmount;
+        public int stoneAmount;
+        public int foodAmount;
+        public int ironAmount;
+        public int woolAmount;
+        public int goldAmount;
+    }
+
+    public List<ResourcesPerSign> resourceListPerSign = new List<ResourcesPerSign>() { };
+
+
+
+    private NewPlayer mainPlayer;
+    public GameObject uiGameobject;
+    public GameObject blockScreen;
+    public TextMeshProUGUI roundText;
+    public GameObject[] signResourcesArray; // Array für die Sign Ressourcen GameObjects
+    public GameObject summaryGameobject;
+    public GameObject detailedGameobject;
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        mainPlayer = TurnManager.Instance.Players.Find(p => p.isMainPlayer);
+        if (mainPlayer == null) return;
+        // Reset
+        ResetList();
+        SetTextForUI();
+        CloseUI();
+    }
+
+    public void GetResourceSummaryList(List<ResourceSummary> resourceSummaries)
+    {
+        foreach (ResourceSummary resourceSummary in resourceSummaries)
+        {
+            if (resourceSummary.ResourceName != ResourceType.XP)
+            {
+                // Prüfen, ob dieser ResourceType schon in der Liste existiert
+                var existing = resourceListPerRoundMainPLayer.Find(r => r.resourceType == resourceSummary.ResourceName);
+                if (existing != null)
+                {
+                    // Amount erhöhen
+                    existing.amount += resourceSummary.Amount;
+                }
+                else { Debug.Log("resourceListPerRoundMainPLayer != null" + resourceSummary.ResourceName); }
+            }
+        }
+        // Sign Ressourcen hinzufügen
+        SetSignResources(resourceSummaries);
+        // Sign Ressourcen Text setzen
+        SetSignRecoucesText();
+    }
+
+    public void SetTextForUI()
+    {
+        roundText.text = "Runde: " + TurnManager.Instance.rundenZähler;
+
+        foreach (ResourcesPerRound resourcesPerRound in resourceListPerRoundMainPLayer)
+        {
+            resourcesPerRound.ressourceField.image.sprite = resourcesPerRound.sprite;
+            resourcesPerRound.ressourceField.text.text = UIResourceSummary.Instance.ZahlenFormatierung(resourcesPerRound.amount);
+        }
+    }
+
+
+    public void OpenUI()
+    {
+        // Set
+        SetTextForUI();
+        // Open UI
+        ChangeToSummaryOverview();
+        //uiGameobject.SetActive(true);
+        blockScreen.SetActive(true);
+        MyEventHandler.Instance.gameMode = GameMode.SkillMenu;
+    }
+
+    public void CloseUI()
+    {
+        // Close UI
+        CloseUIObjects();
+        //uiGameobject.SetActive(false);
+        blockScreen.SetActive(false);
+        MyEventHandler.Instance.gameMode = GameMode.Default;
+        // Reset
+        ResetList();
+        SetTextForUI();
+    }
+
+    public void ChangeToDetailedOverview()
+    {
+        detailedGameobject.SetActive(true);
+        summaryGameobject.SetActive(false);
+    }
+
+    public void ChangeToSummaryOverview()
+    {
+        detailedGameobject.SetActive(false);
+        summaryGameobject.SetActive(true);
+    }
+
+    private void CloseUIObjects()
+    {
+        detailedGameobject.SetActive(false);
+        summaryGameobject.SetActive(false);
+    }
+
+    private void ResetList()
+    {
+        foreach (ResourcesPerRound resourcesPerRound in resourceListPerRoundMainPLayer)
+        {
+            resourcesPerRound.amount = 0;
+        }
+
+        foreach (ResourcesPerSign resourcesPerSign in resourceListPerSign)
+        {
+            resourcesPerSign.woodAmount = 0;
+            resourcesPerSign.stoneAmount = 0;
+            resourcesPerSign.foodAmount = 0;
+            resourcesPerSign.ironAmount = 0;
+            resourcesPerSign.woolAmount = 0;
+            resourcesPerSign.goldAmount = 0;
+        }
+    }
+
+    private void SetSignResources(List<ResourceSummary> resourceSummaries)
+    {
+        Sign currentSign = Dice.Instance.GetSignOfDice();
+
+        ResourcesPerSign resourcesPerSign = resourceListPerSign.Find(r => r.sign == currentSign);
+        if (resourcesPerSign != null)
+        {
+            foreach (ResourceSummary resourceSummary in resourceSummaries)
+            {
+                if (resourceSummary.ResourceName != ResourceType.XP)
+                {
+
+
+                    if (resourceSummary.ResourceName == ResourceType.Wood)
+                    {
+                        resourcesPerSign.woodAmount += resourceSummary.Amount;
+                    }
+                    else if (resourceSummary.ResourceName == ResourceType.Stone)
+                    {
+                        resourcesPerSign.stoneAmount += resourceSummary.Amount;
+                    }
+                    else if (resourceSummary.ResourceName == ResourceType.Food)
+                    {
+                        resourcesPerSign.foodAmount += resourceSummary.Amount;
+                    }
+                    else if (resourceSummary.ResourceName == ResourceType.Iron)
+                    {
+                        resourcesPerSign.ironAmount += resourceSummary.Amount;
+                    }
+                    else if (resourceSummary.ResourceName == ResourceType.Wool)
+                    {
+                        resourcesPerSign.woolAmount += resourceSummary.Amount;
+                    }
+                    else if (resourceSummary.ResourceName == ResourceType.Gold)
+                    {
+                        resourcesPerSign.goldAmount += resourceSummary.Amount;
+                    }
+
+                }
+            }
+        }
+    }
+
+    private void SetSignRecoucesText()
+    {
+        if (signResourcesArray.Length > 0)
+        {
+            int counter = 0;
+
+            foreach (ResourcesPerSign resourcesPerSign in resourceListPerSign)
+            {
+                SignResourceTextHandler signResourceTextHandler = signResourcesArray[counter].GetComponent<SignResourceTextHandler>();
+
+                signResourceTextHandler.woodResourceText.text = UIResourceSummary.Instance.ZahlenFormatierung(resourcesPerSign.woodAmount);
+                signResourceTextHandler.stoneResourceText.text = UIResourceSummary.Instance.ZahlenFormatierung(resourcesPerSign.stoneAmount);
+                signResourceTextHandler.foodResourceText.text = UIResourceSummary.Instance.ZahlenFormatierung(resourcesPerSign.foodAmount);
+                signResourceTextHandler.ironResourceText.text = UIResourceSummary.Instance.ZahlenFormatierung(resourcesPerSign.ironAmount);
+                signResourceTextHandler.woolResourceText.text = UIResourceSummary.Instance.ZahlenFormatierung(resourcesPerSign.woolAmount);
+                signResourceTextHandler.goldResourceText.text = UIResourceSummary.Instance.ZahlenFormatierung(resourcesPerSign.goldAmount);
+
+                counter++;
+            }
+        }
+    }
+}
---
+++ Assets/Child Branch Res Anima/SpriteAnimation/SpriteMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpriteMovement : MonoBehaviour
+{
+    private float moveXRange = 100f; // Bereich für zufällige X-Verschiebung
+    private float moveYRange = 80f; // Bereich für zufällige Y-Verschiebung
+    private ResourceType resourceType;
+    private int amount;
+
+    public AnimationCurve moveCurve; // Animation Curve für die Bewegung
+
+
+    public void StartMovement(Vector3 targtPosition)
+    {
+        StartCoroutine(MoveSprite(targtPosition));
+    }
+
+    private IEnumerator MoveSprite(Vector3 targetPosition)
+    {
+        // Schritt 1: Bewege das Sprite zum vom Spawnpunkt weg
+        Vector3 randomOffset = new Vector3(UnityEngine.Random.Range(-moveXRange, moveXRange), UnityEngine.Random.Range(-moveYRange, moveYRange), 0);
+        yield return new WaitForSeconds(UnityEngine.Random.Range(.1f, .2f));
+        yield return StartCoroutine(MoveOverTime(transform.position, transform.position + randomOffset, UnityEngine.Random.Range(.1f, 1f)));
+
+        // Schritt 2: Bewege das Sprite zum Zielpunkt
+        yield return new WaitForSeconds(UnityEngine.Random.Range(.1f, .4f));
+        yield return StartCoroutine(MoveOverTime(transform.position, targetPosition, UnityEngine.Random.Range(.2f, 3f)));
+
+        //Schritt 3: Wert rechnen
+        yield return StartCoroutine(CalculateValue());
+
+        // Schritt 4: Zerstöre das Sprite nach der Bewegung       
+        Destroy(gameObject);
+    }
+    private IEnumerator MoveOverTime(Vector3 start, Vector3 end, float duration)
+    {
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            // Normierte Zeit von 0 bis 1
+            float t = Mathf.Clamp01(elapsed / duration);
+
+            // AnimationCurve auswerten (z. B. Beschleunigen → Halten → Bremsen)
+            float curveValue = moveCurve.Evaluate(t);
+
+            // Kurvenwert für die Bewegung verwenden
+            transform.position = Vector3.Lerp(start, end, curveValue);
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = end; // Sicherstellen, dass die Endposition genau erreicht wird
+    }
+
+    public void SetObjectStats(ResourceType _resourceType, int _amount)
+    {
+        resourceType = _resourceType;
+        amount = _amount;
+    }
+
+    private IEnumerator CalculateValue()
+    {
+        UIResourceSummary.Instance.UpdateMainPlayerSummaryTextWithAnimationRound(resourceType, amount);
+        yield return null;
+    }
+
+}
